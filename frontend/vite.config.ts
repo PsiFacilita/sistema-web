63ecdca24d5b42140e72bba9a647c2028a4cb88e
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-03-21 18:04:28 -0300

    chore: definindo porta 3000 para utilização do react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
 })
