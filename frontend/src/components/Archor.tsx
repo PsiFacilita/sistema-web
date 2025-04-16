e18ba3952df9ff777c66e9f6a493698f91345986
Gabriel Castro <66182560+castro-bit@users.noreply.github.com>
2025-04-16 19:56:37 -0300

    Create Archor.tsx
    Criando componente ancora

---
+++ frontend/src/components/Archor.tsx
@@ -0,0 +1,50 @@
+//importação do react
+import React from "react";
+
+type AnchorProps = {
+  href: string; //define o destino do link
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void; //Função opcional que é chamada quando o link for clicado.
+  target?: "_self" | "_blank" | "_parent" | "_top"; //self → Abre na mesma aba (padrão), blank → Abre em uma nova aba,parent → Abre na janela/página pai ,top → Abre na janela principal (caso esteja dentro de um iframe).
+  rel?: string; // Define a relação entre o documento e o destino do link. Em links externos, pode ser usado para segurança, como "noopener noreferrer".
+  disabled?: boolean; //Define se o link está desativado.
+  className?: string; // Permite adicionar classes CSS para estilizar o link.
+  style?: React.CSSProperties; //Permite adicionar estilos inline ao link.
+  children: React.ReactNode; //Representa o conteúdo do link (pode ser um texto ou até ícones).
+};
+
+const Anchor: React.FC<AnchorProps> = ({
+  href,
+  onClick,
+  target = "_self",
+  rel,
+  disabled = false,
+  className = "",
+  style = {},
+  children,
+}) => {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (disabled) {
+      event.preventDefault();
+      return;
+    }
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <a
+      href={disabled ? "#" : href}
+      onClick={handleClick}
+      target={target}
+      rel={rel}
+      className={`${className} ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
+      style={style}
+      aria-disabled={disabled}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Anchor;
