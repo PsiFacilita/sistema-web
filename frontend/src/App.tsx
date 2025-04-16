e85365546420cf928e3591ba0f97bcd54f051fb4
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-16 16:47:52 -0300

    feat: criando página para testar visualmente o dropdown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+import TestDropdownPage from './pages/TestDropdownPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/dropdown" element={<TestDropdownPage />} />
       </Routes>
     </Router>
   );
