727254db8058ad61461dbd23c788996968e5030a
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 19:33:22 -0300

    feat: criando página para testar diferentes estados da ancora

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+import AnchorPage from './pages/AnchorPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/anchor" element={<AnchorPage />} />
       </Routes>
     </Router>
   );
