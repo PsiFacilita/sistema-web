4cc1fe314a507f9c7171e3047f3e20493688fe90
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 19:51:31 -0300

    fix: corrigindo página para habilitar merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 
-import ListPage from './pages/ListPage';
-
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        
-        <Route path="/list" element={<ListPage />} />
       </Routes>
     </Router>
   );
