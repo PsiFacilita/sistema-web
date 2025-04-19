3daeac267f0eeaa7378570950f154948896c790c
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 19:03:46 -0300

    refactor: removendo AnchorProps do arquivo principal

--- frontend/src/components/Anchor.tsx
+++ frontend/src/components/Anchor.tsx
@@ -1,18 +1,6 @@
 import React from "react";
 import { AnchorProps } from "./Anchor.types";
 
-
-type AnchorProps = {
-  href: string; //define o destino do link
-  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void; //Função opcional que é chamada quando o link for clicado.
-  target?: "_self" | "_blank" | "_parent" | "_top"; //self → Abre na mesma aba (padrão), blank → Abre em uma nova aba,parent → Abre na janela/página pai ,top → Abre na janela principal (caso esteja dentro de um iframe).
-  rel?: string; // Define a relação entre o documento e o destino do link. Em links externos, pode ser usado para segurança, como "noopener noreferrer".
-  disabled?: boolean; //Define se o link está desativado.
-  className?: string; // Permite adicionar classes CSS para estilizar o link.
-  style?: React.CSSProperties; //Permite adicionar estilos inline ao link.
-  children: React.ReactNode; //Representa o conteúdo do link (pode ser um texto ou até ícones).
-};
-
 const Anchor: React.FC<AnchorProps> = ({
   href,
   onClick,
