75eb7ebd4198ba066555ea578a8d3462d6ec5d8b
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 20:48:14 -0300

    feat: importando página de exibição

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import AnchorPage from './pages/AnchorPage';
+import ListPage from './pages/ListPage';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/anchor" element={<AnchorPage />} />
+        <Route path="/list" element={<ListPage />} />
       </Routes>
     </Router>
   );
