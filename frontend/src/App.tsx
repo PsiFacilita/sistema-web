42a941a5bc361ace0109b6c3a2c26d97eb918e86
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 19:50:39 -0300

    fix: corrigindo linhas da página

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+
 import ListPage from './pages/ListPage';
 
 const App: React.FC = () => {
@@ -9,6 +10,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        
         <Route path="/list" element={<ListPage />} />
       </Routes>
     </Router>
