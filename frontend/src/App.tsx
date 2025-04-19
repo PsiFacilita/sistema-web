26296e54fe23ab8473eafd81d15a64ccc172b74e
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-04-19 19:49:48 -0300

    feat: criando página com um caso de uso para componente de Lista

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+import ListPage from './pages/ListPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/list" element={<ListPage />} />
       </Routes>
     </Router>
   );
