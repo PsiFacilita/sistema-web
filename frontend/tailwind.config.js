9d0a734aef6bcdcc0ee88d959c417959db93c521
MelissaMaximo <129124909+MelissaMaximo@users.noreply.github.com>
2025-04-24 14:32:44 -0300

    Atualizando tailwind com cores personalizadas

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#245B36',
+          dark: '#1A4530'
+        },
+        danger: {
+          DEFAULT: '#903A1D',  
+          dark: '#7A2E16'      
+        }
+      }
+    },
   },
   plugins: [],
 }
