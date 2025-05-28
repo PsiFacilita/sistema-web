7446b14d52f937f9fa6dc0d65b5492976ac33005
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-05-28 12:11:34 -0300

    fix: merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
 
 import Dashboard from './pages/Dashboard';
+import Documents from './pages/Documents';
+import Appointments from './pages/Appointments';
 import Settings from './pages/Settings';
-import LoginPage from './pages/LoginPage';
+import PatientRecord from './pages/PatientRecord';
+import Login from './pages/Login';
+import Patients from './pages/Patients';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/records" element={<PatientRecord />} />
+            <Route path="/patients" element={<Patients />} />
+          </Routes>
+        </AuthProvider>
     </Router>
   );
 };
