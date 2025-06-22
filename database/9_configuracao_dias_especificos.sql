af87d8598b814e4be4125d4fd937b53fb46226f1
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-06-22 19:15:43 -0300

    fix: table name to configuracao_dias_especificos

--- database/9_configuracao_dias_especificos.sql
+++ database/9_configuracao_dias_especificos.sql
@@ -1,3 +1,3 @@
-INSERT INTO configuracao_dia_especificos (configuracao_id, data, tipo, motivo)
+INSERT INTO configuracao_dias_especificos (configuracao_id, data, tipo, motivo)
 VALUES
 (1, '2025-07-01', 'fechado', 'Feriado municipal');
