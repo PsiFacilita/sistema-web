67f4791f503fcc3896a2953bbdfa29cbad2cf20c
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-06-26 16:44:11 -0300

    refactor: update routes and add patientview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import DocumentViewer from './pages/DocumentViewer';
 import Appointments from './pages/Appointments';
 import Settings from './pages/Settings';
 import PatientRecord from './pages/PatientRecord';
+import PatientView from './pages/PatientView';
 import Login from './pages/Login';
 import Patients from './pages/Patients';
 import Help from './pages/Help';
@@ -16,17 +17,24 @@ import PasswordReset from './pages/PasswordReset';
 const App: React.FC = () => {
   return (
     <Router>
-
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Login />} />
+
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/appointments" element={<Appointments />} />
-            <Route path="/documents" element={<Documents />} />
-            <Route path="/documents/:id/view" element={<DocumentViewer />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/records" element={<PatientRecord />} />
+
             <Route path="/patients" element={<Patients />} />
+            <Route path="/patients/:id" element={<PatientView />} />
+            <Route path="/records/:id" element={<PatientRecord />} />
+
+
+            <Route path="/appointments" element={<Appointments />} />
+
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/documents/:id" element={<DocumentViewer />} />
+
+            <Route path="/settings" element={<Settings />} />
+
             <Route path="/help" element={<Help />} />
 
             <Route path="/password-reset" element={<PasswordReset />} />
