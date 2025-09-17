c44774b388e0dd5946fa0811409eec968be24be9
Gabriel Mioni <104784563+gab3mioni@users.noreply.github.com>
2025-09-16 22:07:10 -0300

    fix: use middleware

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use App\Http\Middleware\RateLimitMiddleware;
 use Slim\Factory\AppFactory;
 use Symfony\Component\Dotenv\Dotenv;
 use App\Http\Middleware\CorsMiddleware;
